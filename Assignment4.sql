a2a561efefc9701f2c95285cff9f17f37fd114f1
nirajprasad12 <nirajsaiprasad@gmail.com>
2022-04-11 07:18:53 -0400

    almost final version of release

--- Assignment4.sql
+++ Assignment4.sql
@@ -1,25 +1,25 @@
 /*
-A. CREATE DEPT TABLE AND INSERT 6 RECORDS INTO IT (refer to Instructions for Schema) - 6 points
+A. CREATE DEPT TABLE AND INSERT 6 RECORDS INTO IT (refer to Instructions for Schema) - 6 points DONE
 
-B. INSERT THE DEPARTMENT IF NAME DOESN'T EXISTS - 6 points
+B. INSERT THE DEPARTMENT IF NAME DOESN'T EXISTS - 6 points DONE
 
-C. UPDATE THE DEPARTMENT LOCATION IF NAME EXISTS - 6 points
+C. UPDATE THE DEPARTMENT LOCATION IF NAME EXISTS - 6 points DONE
 
-D. RAISE ERROR IF THE DEPARTMENT NAME IS INVALID (NULL, ZERO LENGTH) - 6 points
+D. RAISE ERROR IF THE DEPARTMENT NAME IS INVALID (NULL, ZERO LENGTH) - 6 points DONE
 
 E. RAISE ERROR IF THE DEPARTMENT NAME IS A NUMBER - 6 points
 
-F. ACCEPTED LOCATIONS SHOULD BE AS BELOW - 6 points
+F. ACCEPTED LOCATIONS SHOULD BE AS BELOW - 6 points DONE
 
                 MA, TX, IL, CA, NY, NJ, NH, RH
 
-G. DEPARTMENT ID SHOULD BE AUTO-GENERATED - 6 points
+G. DEPARTMENT ID SHOULD BE AUTO-GENERATED - 6 points DONE
 
-H. LENGTH OF THE DEPARTMENT NAME CANNOT BE MORE THAN 20 CHARS - 6 points
+H. LENGTH OF THE DEPARTMENT NAME CANNOT BE MORE THAN 20 CHARS - 6 points DONE
 
-I. WHILE INSERTING THE DEPARTMENT NAME CONVERT EVERYTHING TO CAMEL CASE - 6 points
+I. WHILE INSERTING THE DEPARTMENT NAME CONVERT EVERYTHING TO CAMEL CASE - 6 points DONE
 
-J. MAKE SURE DEPARTMENT NAME IS UNIQUE - 6 points
+J. MAKE SURE DEPARTMENT NAME IS UNIQUE - 6 points DONE
 
 */
 
@@ -79,18 +79,63 @@ end;
 
 create or replace procedure insert_department(in_dept_name department.dept_name%TYPE, in_dept_location department.dept_location%TYPE)
 is
-v_cnt number;
+v_NameExists number;
+v_deptLength number;
+
+queryCheck varchar(200);
+
+
+e_input_length exception;
+e_accepted_locations exception;
 begin
-    execute immediate('insert into department(dept_name, dept_location) values(initcap('''|| in_dept_name ||'''),upper('''|| in_dept_location ||''') )');
-    dbms_output.put_line('Running stored proc');
+    
+    v_NameExists := 0;
+    
+    select length(in_dept_name) into v_deptLength from dual;
+    
+    if v_deptlength > 20 then
+        raise e_input_length;
+    end if;
+    
+    if upper(in_dept_location) not in ('MA', 'TX', 'IL', 'CA', 'NY', 'NJ', 'NH', 'RH') then
+        raise e_accepted_locations;
+    end if;
+    
+    select count(*) into v_NameExists from department where dept_name = initcap(in_dept_name);
+    if v_NameExists = 0 then
+        execute immediate('insert into department(dept_name, dept_location) values(initcap('''|| in_dept_name ||'''),upper('''|| in_dept_location ||''') )');
+        
+        dbms_output.put_line('New department inserted');
+        commit;
+    else
+        execute immediate('update department set dept_location = upper(''' || in_dept_location || ''') where dept_name = initcap('''|| in_dept_name ||''')');
+        dbms_output.put_line(queryCheck);
+        dbms_output.put_line('Department already exists, it must be unique, updating the location only..');
+        commit;
+    end if;
+
+exception
+    when e_accepted_locations then
+        dbms_output.put_line('Error: Allowed locations are MA, TX, IL, CA, NY, NJ, NH, RH');
+    
+    when e_input_length then
+        dbms_output.put_line('Error: Allowed Max string length of 20 for Dept_Name');
+        
+    when others then   
+        if sqlcode = -1400 then
+            dbms_output.put_line('Do not enter NULL/Empty values!!');
+        elsif sqlcode = -6550 then
+            dbms_output.put_line('Enter correct number of argumentS!!');
+        else
+            dbms_output.put_line('Operation not performed!! Contact Admin!!');
+        end if;
+    rollback;
 end;
 /
 
+exec insert_department('This is a long department', 'Nj');
 
-exec insert_department('K', 'L')
-
-
-
+select * from department;
 
 
 
